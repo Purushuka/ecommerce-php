f638aa3ce9773919ecb63a2498170bbcdfed502b
Purushuka <dima.griniky@gmail.com>
2022-01-20 21:25:12 +0300

    Добавлен функционал: Добавлять новые категории и товары, добавлена проверка при добавлении записей в таблицу

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
+const GET_REQUEST = 'GET';
+const POST_REQUEST = 'POST';
+
+const API_GET_CATEGORIES = 'categories';
+const API_GET_PRODUCTS   = 'products';
+const API_POST_CATEGORIES = 'postCategories';
+const API_POST_PRODUCTS   = 'postProducts';
+
+
 /**
  * Возвращает данные о запросе
  *
@@ -12,10 +21,32 @@ $dotenv->load();
  */
 function getRequest(): array
 {
-    return [
-        'method' => array_shift($_GET), // null или string
-        'data'   => $_GET                     // null или array
+    $method = $_REQUEST['method'] ?? null;
+    unset($_REQUEST['method']);
+
+    $request = [
+        'method' => $method, // null или string
+        'data' => $_REQUEST  // null или array
     ];
+
+    if ($_SERVER['REQUEST_METHOD'] === GET_REQUEST) {
+        ////////
+    } elseif ($_SERVER['REQUEST_METHOD'] === POST_REQUEST) {
+
+        switch ($_REQUEST['type']) {
+            case 'category':
+                $request['method'] = API_POST_CATEGORIES;
+                break;
+
+            case 'product':
+                $request['method'] = API_POST_PRODUCTS;
+                break;
+            default:
+
+                break;
+        }
+    }
+    return $request;
 }
 
 /**
@@ -34,7 +65,8 @@ function getDatabaseConnection(): PDO
 
     if ($pdo = new PDO($mysql, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'])) {
         return $pdo;
-    } else {
+    }
+    else {
         die('Database connection failed!');
     }
 }
--- app/index.php
+++ app/index.php
@@ -3,64 +3,77 @@
 // http://localhost:8000/?method=categories&id=3&products=true
 
 // Инициализация:
-// 1. Получить данные из файлов /datasets/categories.php и /datasets/products.php
-// 2. Обрабатываешь их и если таких записей нет в базе, то добавляешь
+// [x] 1. Получить данные из файлов /datasets/categories.php и /datasets/products.php
+// [x] 2. Обрабатываешь их и если таких записей нет в базе, то добавляешь
 
-// 1. Заполнить функции getCategories и getProducts
+// [x] 1. Заполнить функции getCategories и getProducts
+// [ ] 2. На каждый ендпоинт сделать возможность добавлять данные (новые категории, товары)
 
 require_once __DIR__ . '/bootstrap.php';
 
-const API_CATEGORIES = 'categories';
-const API_PRODUCTS   = 'products';
-
 $request = getRequest();
-$pdo     = getDatabaseConnection();
+$pdo = getDatabaseConnection();
 
 fillDatabase($pdo);
 
+function isRecordExist(string $table, string $column, string $value, PDO $pdo): bool
+{
+    $query = sprintf('SELECT * FROM %s WHERE %s=:value', $table, $column);
+    $statment = $pdo->prepare($query);
+    $statment->execute(['value' => $value]);
+
+    return !empty($statment->fetchAll());
+}
+
 function fillDatabase(PDO $pdo)
 {
-    $categories = require __DIR__.'/datasets/categories.php';
-    $products = require __DIR__.'/datasets/products.php';
-    $existProducts = $pdo->prepare('SELECT * FROM products WHERE title=:title');
-    $existCategories = $pdo->prepare('SELECT * FROM categories WHERE title=:title');
+    $categories = require __DIR__ . '/datasets/categories.php';
+    $products = require __DIR__ . '/datasets/products.php';
 
     foreach ($products as $product) {
-        $existProducts->execute([
-            'title' => $product['title']
-        ]);
-
-        if (! $existProducts->fetch()) {
-            addProduct($product,$pdo);
+        if (!isRecordExist('products', 'title', $product['title'], $pdo)) {
+            addProduct($product, $pdo);
         }
     }
 
     foreach ($categories as $category) {
-        $existCategories->execute([
-            'title' => $category['title']
-        ]);
-
-        if (! $existCategories->fetch()) {
-            addCategory($category,$pdo);
+        if (!isRecordExist('categories', 'title', $category['title'], $pdo)) {
+            addCategory($category, $pdo);
         }
     }
 }
 
 function addCategory(array $category, PDO $pdo): void
 {
-    $addCategory = $pdo->prepare('INSERT INTO categories (title) VALUES(:title)');
-    $addCategory->execute([
-         'title' => $category['title']
-    ]);
+    if (!isRecordExist('categories', 'title', $category['title'], $pdo)) {
+        $addCategory = $pdo->prepare('INSERT INTO categories (title) VALUES(:title)');
+        $addCategory->execute([
+            'title' => $category['title']
+        ]);
+    }
+    else {
+        echo 'Категория с таким' . $category['title'] . 'уже существует';
+    }
+
 }
 
 function addProduct(array $product, PDO $pdo): void
 {
-    $addProduct = $pdo->prepare('INSERT INTO products (title, category_id) VALUES(:title, :category_id)');
-    $addProduct->execute([
-        'title' => $product['title'],
-        'category_id' => $product['category_id']
-    ]);
+
+    if (!isRecordExist('products', 'title', $product['title'], $pdo)) {
+        if (isset($product['category_id']) && isRecordExist('categories', 'id', $product['category_id'], $pdo )) {
+            $addProduct = $pdo->prepare('INSERT INTO products (title, category_id) VALUES(:title, :category_id)');
+            $addProduct->execute([
+                'title' => $product['title'],
+                'category_id' => $product['category_id'] ?? null
+            ]);
+        } else {
+            echo 'Категория с ID: ' . $product['category_id'] . ' не существует';
+        }
+    }
+    else {
+        echo 'Продукт с таким' . $product['title'] . 'уже существует';
+    }
 }
 
 function getCategories(?array $data): array
@@ -68,36 +81,36 @@ function getCategories(?array $data): array
     global $pdo;
     // Возможные параметры:
     // [x]id - ?int идентификатор категории => Вернуть данные о категории под этим id, если она существует
-    // products - bool (false по умолчанию), требует id если передан true => Вернуть товары из категории по id
+    // [x]products - bool (false по умолчанию), требует id если передан true => Вернуть товары из категории по id
     // [x] Если никакие параметры не переданы вернуть список всех категорий
 
     // Пример корректных параметров $data = ['id' => '3', 'products' => 'true']
     //        $getCategories = $pdo->prepare('SELECT * FROM categories WHERE id =:category_id');
-    if($data['products']===true){
+
+    if (isset($data['products']) && $data['products'] === 'true') {
         if (isset($data['id'])) {
-            $getProducts = $pdo->prepare('SELECT * FROM products WHERE id =:category_id');
+            $getProducts = $pdo->prepare('SELECT * FROM products WHERE category_id =:category_id');
             $getProducts->execute([
-                'category_id' => $data['category_id']
+                'category_id' => $data['id']
             ]);
+            return fetchAll($getProducts);
         }
         else
             echo 'НУ введи id плз';
 
     }
-    if (isset($data['id'])) {
+    elseif (isset($data['id'])) {
         $getCategories = $pdo->prepare('SELECT * FROM categories WHERE id =:category_id');
         $getCategories->execute([
             'category_id' => $data['id']
         ]);
 
-        return $getCategories->fetchAll();
+        return fetchAll($getCategories);
     }
-
 
     $getCategories = $pdo->prepare('SELECT * FROM categories ');
     $getCategories->execute();
-    return $getCategories->fetchAll();
-
+    return fetchAll($getCategories);
 }
 
 function getProducts(?array $data): array
@@ -110,27 +123,33 @@ function getProducts(?array $data): array
             'product_id' => $data['id']
         ]);
 
-        return $getProducts->fetchAll();
+        return fetchAll($getProducts);
     }
 
     $getProducts = $pdo->prepare('SELECT * FROM products ');
     $getProducts->execute();
-    return $getProducts->fetchAll();
+    return fetchAll($getProducts);
+}
 
-    // Возможные параметры:
-    // [x] id - ?int идентификатор товара => Вернуть данные о товаре под этим id, если он существует
-    // [x]  Если никакие параметры не переданы вернуть список всех товаров
-
-    // Пример корректных параметров $data = ['id' => '33']
+function fetchAll(PDOStatement $statement): array
+{
+    return $statement->fetchAll(PDO::FETCH_NAMED);
 }
 
 switch ($request['method']) {
-    case API_CATEGORIES:
+    case API_GET_CATEGORIES:
         $result = getCategories($request['data']);
         break;
-    case API_PRODUCTS:
+    case API_GET_PRODUCTS:
         $result = getProducts($request['data']);
         break;
+    case API_POST_CATEGORIES:
+        $result = addCategory($request['data'], $pdo);
+        break;
+    case API_POST_PRODUCTS:
+        $result = addProduct($request['data'], $pdo);
+        break;
+
     default:
         //
         break;
@@ -139,3 +158,58 @@ switch ($request['method']) {
 if (isset($result)) {
     var_dump($result);
 }
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div style="width: 400px; margin: 0 auto">
+    <form action="index.php" method="post">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Awesome Title">
+        </div>
+        <div class="form-group">
+            <label for="category_id">Category ID</label>
+            <input type="number" class="form-control" id="category_id" name="category_id" placeholder="3" min="1">
+        </div>
+        <div class="form-group">
+            <label for="type">Type</label>
+            <select class="form-control" id="type" name="type">
+                <option value="category">Category</option>
+                <option value="product">Product</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <input type="submit">
+        </div>
+    </form>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
+
